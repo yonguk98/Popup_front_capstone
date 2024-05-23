151ea4202e900d480976f43a1b52937aab27b99c
yonguk <kimyu9827@naver.com>
2024-05-24 06:19:55 +0900

    feature: add crawling button

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -111,7 +111,7 @@ export default {
         };
 
         const crawlingRequest = async () => {
-            const response = await fetch(`${baseUrl}/admin/store/crawling`, {
+            const response = await fetch(`${baseUrl}/admin/store/crawling?accountName=sungsu_bible`, {
                     method: 'GET',
                     credentials: 'include',
                     headers: {
